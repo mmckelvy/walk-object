41f1538b808fdfc678291b55cca217b9cc5fad88
Mark McKelvy <mmckelvy@gmail.com>
2017-03-25 16:43:37 -0700

    add location for objects; better test

--- test/walk-object.test.js
+++ test/walk-object.test.js
@@ -4,40 +4,154 @@ const walkObject = require('../walk-object')
 
 test('walkObject', function(t) {
   const obj = {
-    fruit: {
-      description: 'apple',
-      color: [
+    order: {
+      number: 243,
+      customer: {
+        name: 'John Smith',
+        email: 'j@smith.com',
+        city: 'LA'
+      },
+      items: [
         {
-          description: 'red',
-          types: [
-            {description: 'fuji'},
-            {description: 'golden'},
-          ],
-          sizes: [
-            {description: 'large'},
-            {description: 'small'}
-          ],
+          fruit: 'apple',
+          color: 'red',
           regions: [
-            {description: 'WA'},
-            {description: 'OR'},
+            {
+              state: 'Washington',
+              city: 'Redmond'
+            },
+            {
+              state: 'Maine',
+              city: 'Portland'
+            }
+          ]
+        },
+        {
+          fruit: 'grape',
+          color: 'purple',
+          regions: [
+            {
+              state: 'California',
+              city: 'Sonoma'
+            },
+            {
+              state: 'Oregon',
+              city: 'Willamette'
+            }
           ]
         }
       ]
     }
   }
 
-  const results = []
-
-  const fn = function(val) {
-    results.push(val)
+  // Log both values and locations
+  const valuesAndLocations = []
+  const logValuesAndLocations = function(value, location) {
+    valuesAndLocations.push({value, location})
   }
 
-  walkObject(obj, fn),
+  walkObject(obj, logValuesAndLocations)
+
+  // Log values only
+  const values = []
+  const logValues = function(value) {
+    values.push(value)
+  }
+
+  walkObject(obj, logValues)
 
   t.deepEqual(
-    results,
-    ['apple', 'red', 'fuji', 'golden', 'large', 'small', 'WA', 'OR'],
+    valuesAndLocations,
+    [
+      {
+        value: 243,
+        location: ['order', 'number']
+      },
+      {
+        value: 'John Smith',
+        location: ['order', 'customer', 'name']
+      },
+      {
+        value: 'j@smith.com',
+        location: ['order', 'customer', 'email']
+      },
+      {
+        value: 'LA',
+        location: ['order', 'customer', 'city']
+      },
+      {
+        value: 'apple',
+        location: ['order', 'items', 0, 'fruit']
+      },
+      {
+        value: 'red',
+        location: ['order', 'items', 0, 'color']
+      },
+      {
+        value: 'Washington',
+        location: ['order', 'items', 0, 'regions', 0, 'state']
+      },
+      {
+        value: 'Redmond',
+        location: ['order', 'items', 0, 'regions', 0, 'city']
+      },
+      {
+        value: 'Maine',
+        location: ['order', 'items', 0, 'regions', 1, 'state']
+      },
+      {
+        value: 'Portland',
+        location: ['order', 'items', 0, 'regions', 1, 'city']
+      },
+      {
+        value: 'grape',
+        location: ['order', 'items', 1, 'fruit']
+      },
+      {
+        value: 'purple',
+        location: ['order', 'items', 1, 'color']
+      },
+      {
+        value: 'California',
+        location: ['order', 'items', 1, 'regions', 0, 'state']
+      },
+      {
+        value: 'Sonoma',
+        location: ['order', 'items', 1, 'regions', 0, 'city']
+      },
+      {
+        value: 'Oregon',
+        location: ['order', 'items', 1, 'regions', 1, 'state']
+      },
+      {
+        value: 'Willamette',
+        location: ['order', 'items', 1, 'regions', 1, 'city']
+      },
+    ],
     'Should walk the object'
+  )
+
+  t.deepEqual(
+    values,
+    [
+      243,
+      'John Smith',
+      'j@smith.com',
+      'LA',
+      'apple',
+      'red',
+      'Washington',
+      'Redmond',
+      'Maine',
+      'Portland',
+      'grape',
+      'purple',
+      'California',
+      'Sonoma',
+      'Oregon',
+      'Willamette',
+    ],
+    'Should work with values only'
   )
 
   t.end()
